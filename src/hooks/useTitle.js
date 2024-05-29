d31326b0fc64374ef87ea2f94dcedb0097d34cd5
Gerson Sanchez <gealsanchez@gmail.com>
2024-05-29 12:44:02 -0500

    Take one

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export const useTitle = (title) => {
     useEffect(() => {
-        document.title = `${title} / #INCOGNITO`;
+        document.title = `${title} / Valencia Floristeria`;
     })
     return null
 }
