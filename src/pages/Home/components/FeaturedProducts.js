dac9dc6b4cfcc60b86b8700b389e04a5d964c3ae
Gerson Sanchez <gealsanchez@gmail.com>
2024-06-04 13:23:50 -0500

    image updated

--- src/pages/Home/components/FeaturedProducts.js
+++ src/pages/Home/components/FeaturedProducts.js
@@ -23,7 +23,7 @@ export const FeaturedProducts = () => {
 
     return (
         <section className="my-20">
-            <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5">New Features</h1>
+            <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5">Novedades</h1>
             <div className="flex flex-wrap justify-center lg:flex-row">
                 {loading && <div className="text-center">
                     <div role="status">
