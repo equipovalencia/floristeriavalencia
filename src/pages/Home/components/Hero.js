2514ae816d899b5bf3a6afa70bdf60547ced7972
Gerson Sanchez <gealsanchez@gmail.com>
2024-06-04 13:34:28 -0500

    image updated

--- src/pages/Home/components/Hero.js
+++ src/pages/Home/components/Hero.js
@@ -4,7 +4,7 @@ export const Hero = () => {
     return (
         <section className="flex flex-col lg:flex-row dark:text-slate-100 items-center">
             <div className="text my-5">
-                <h1 className="text-5xl font-bold">Relieves y Floristeria</h1>
+                <h1 className="text-4xl font-bold">Relieves y Floristeria</h1>
                 <p className="text-2xl my-7 px-1 dark:text-slate-300">Lapidas en aluminio, acrílico y mármol, avisos, logotipos, medallas, señalizaciones, nomenclaturas, placas conmemorativas, sellos para calzado, arreglos florales para toda ocasión, visítenos para tener el gusto de atenderle</p>
                 <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Nuestros Productos</Link>
             </div>
