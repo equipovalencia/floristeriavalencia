10272936824e38cae57c0cdad36222ec86ea78ba
Gerson Sanchez <gealsanchez@gmail.com>
2024-06-04 13:11:23 -0500

    image updated

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export const useTitle = (title) => {
     useEffect(() => {
-        document.title = `${title} / Valencia Floristeria`;
+        document.title = `${title} / Floristeria Valencia`;
     })
     return null
 }
