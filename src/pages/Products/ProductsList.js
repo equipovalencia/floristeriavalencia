c703a249acfc2d47da44c7654fdc8b28c0a48db9
Gerson Sanchez <gealsanchez@gmail.com>
2024-06-04 13:42:06 -0500

    image updated

--- src/pages/Products/ProductsList.js
+++ src/pages/Products/ProductsList.js
@@ -35,7 +35,7 @@ export const ProductsList = () => {
     <main>
       <section className="my-5">
         <div className="my-5 flex justify-between">
-          <span className="text-2xl font-semibold dark:text-slate-100 mb-5">All Features ({productList.length})</span>
+          <span className="text-2xl font-semibold dark:text-slate-100 mb-5">Productos ({productList.length})</span>
           <span>
 
             <button onClick={() => setShow(!show)} id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots" className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-700" type="button">
